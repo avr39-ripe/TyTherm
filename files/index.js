3772dc68d0575b795b47e6e1f63663ceea0bdff9
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-02-27 20:49:26 +0200

    Display _healthy status in temperature-panel by color

--- files/index.js
+++ files/index.js
@@ -4,6 +4,15 @@ $( document ).ready(function() {
 		$.getJSON('/state', function(data) {
 			document.getElementById('counter').textContent = data.counter;
 			document.getElementById('temperature').innerHTML = data.temperature + ' &deg;C';
+			var tempPanel = document.getElementById('temperature-panel');
+			if (data.healthy) {
+				tempPanel.classList.remove("panel-danger");
+				tempPanel.classList.add("panel-default");
+			}
+			else {
+				tempPanel.classList.remove("panel-default");
+				tempPanel.classList.add("panel-danger");
+			}
 			setTimeout(worker, 5000);
 		});
 	})();
